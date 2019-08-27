72b2c2f46db18fdb5a9a467f8d46c300b2601e41
David Piper <black.god1930@gmail.com>
2019-08-27 04:52:44 -0400

    Updates

---
+++ precache-manifest.82f8ebcf76964a93b489b45d56fb9567.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4fd75d7b1f1a48059548abdea1663e",
+    "url": "/http--02-starting-setup/index.html"
+  },
+  {
+    "revision": "dc6672bf1e9318348812",
+    "url": "/http--02-starting-setup/static/css/3.c473469c.chunk.css"
+  },
+  {
+    "revision": "f7d6f7332679fc22f6d0",
+    "url": "/http--02-starting-setup/static/css/main.8c58686e.chunk.css"
+  },
+  {
+    "revision": "0a5eb9ff80a28238045f",
+    "url": "/http--02-starting-setup/static/js/2.8831fbd1.chunk.js"
+  },
+  {
+    "revision": "dc6672bf1e9318348812",
+    "url": "/http--02-starting-setup/static/js/3.8ccb1f82.chunk.js"
+  },
+  {
+    "revision": "f7d6f7332679fc22f6d0",
+    "url": "/http--02-starting-setup/static/js/main.6226557b.chunk.js"
+  },
+  {
+    "revision": "7412183fb204f8cf9c0f",
+    "url": "/http--02-starting-setup/static/js/runtime~main.0b02b29e.js"
+  }
+]);
